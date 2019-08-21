9ba4d7047f22ef30ba79cf93809b07d4d2297c86
Léu Almeida <leo@webid.net.br>
2019-08-20 21:45:01 -0300

    Initial commit

---
+++ android/app/src/main/java/br/net/webid/primeiro_projeto/MainActivity.java
@@ -0,0 +1,13 @@
+package br.net.webid.primeiro_projeto;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
